1adfa0affac8f9abc64538ceb78f5fd13f0957b5
naveen <naveenkumar.p175@gmail.com>
2022-01-31 17:44:08 +0530

    initial commit

---
+++ src/main/java/com/myapp/user/reward/UserRewardsApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.user.reward;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRewardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/user/reward/UserRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.user.reward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
